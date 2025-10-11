5bc9f1b04a4160514d902d44aa5936aa87c232af
SchubideiCroissant <151747990+SchubideiCroissant@users.noreply.github.com>
2025-10-11 14:46:35 +0200

    added keystore pw

--- src/main/java/homeMusic/Config/DotenvLoader.java
+++ src/main/java/homeMusic/Config/DotenvLoader.java
@@ -14,16 +14,18 @@ public class DotenvLoader implements ApplicationContextInitializer<ConfigurableA
         String ip = dotenv.get("YOUR_IP", "");
         String clientId = dotenv.get("SPOTIFY_CLIENT_ID", "");
         String clientSecret = dotenv.get("SPOTIFY_CLIENT_SECRET", "");
+        String keystore_pw =  dotenv.get("KEYSTORE_PASSWORD", "");
 
         // set in .properties
         System.setProperty("YOUR_IP", ip);
         System.setProperty("SPOTIFY_CLIENT_ID", clientId);
         System.setProperty("SPOTIFY_CLIENT_SECRET", clientSecret);
+        System.setProperty("KEYSTORE_PASSWORD", keystore_pw);
 
         // Debug
         System.out.println("[.env] YOUR_IP=" + ip);
         System.out.println("[.env] SPOTIFY_CLIENT_ID=" + clientId);
         System.out.println("[.env] SPOTIFY_CLIENT_SECRET=" + (clientSecret.isEmpty() ? "(leer)" : "********"));
-
+        System.out.println("[.env] KEYSTORE_PASSWORD=" + (keystore_pw.isEmpty() ? "(leer)" : "********"));
     }
 }
